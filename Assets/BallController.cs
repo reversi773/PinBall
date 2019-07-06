94dccf700e8c8f9d5b5a2b43ba9f4089c174fd4c
reversi773 <reversi-reverever324@docomo.ne.jp>
2019-07-06 18:46:06 +0900

    PinBallプロジェクトのコミット。
    ピンボールの3Dゲームを作ったファイルです。

---
+++ Assets/BallController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+   
+    GameObject director;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+        //シーン中のScoreTextオブジェクトを取得
+        this.director = GameObject.Find("ScoreText");
+    }
+    //Physicsが登録されているオブジェクトから衝突時に呼び出す
+    private void OnCollisionEnter(Collision other)
+      {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+
+            this.director.GetComponent<ScoreController>().ClashSmallStar();
+           
+        }
+        else if(other.gameObject.tag == "LargeStarTag")
+        {
+            this.director.GetComponent<ScoreController>().ClashLargeStar();
+          
+        }
+        else if (other.gameObject.tag =="SmallCloudTag")
+        {
+            this.director.GetComponent<ScoreController>().ClashSmallCloud();
+
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            this.director.GetComponent<ScoreController>().ClashLargeCloud();
+       
+        }
+
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+}
+
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+    //得点を表示するテキスト
+    private GameObject ScoreText;
+    //score変数を設定
+    int score = 0;
+    //各オブジェクトと衝突した際の得点の値を設定
+    public void ClashSmallCloud()
+    {
+        this.score += 30;
+        Debug.Log(score);
+    }
+    public void ClashLargeCloud()
+    {
+        this.score += 10;
+        Debug.Log(score);
+    }
+    public void ClashSmallStar ()
+    {
+        this.score += 40;
+        Debug.Log(score);
+    }
+    public void ClashLargeStar()
+    {
+        this.score += 20;
+        Debug.Log(score);
+    }
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        //シーン中のScoreTextオブジェクトを取得
+        this.ScoreText = GameObject.Find("ScoreText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.ScoreText.GetComponent<Text>().text = this.score.ToString() + "score";
+      
+		
+	}
+}
